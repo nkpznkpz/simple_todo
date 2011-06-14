3cbc7893ee1996a5a05d72afd88dbc01b5f83fbc
Palakon <my_name_is_n@hotmail.com>
2011-06-14 20:39:04 +0700

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleTodo::Application.config.secret_token = '739db4edbbc302644831f4cf60947cff55d7a054a982052ea7159ab6da8add2ff7182ecba1d592c4f4622beccc2f12cf18b3ba4ae1a15c133cb6b9bb526b6239'
